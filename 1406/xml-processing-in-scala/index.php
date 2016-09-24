590253e6b9fe113d1a172821e2113b18df66b678
Vykintas Narmontas (William) <me@vynar.com>
2016-09-24 09:31:35 +0800

    Create index.php

---
+++ 1406/xml-processing-in-scala/index.php
@@ -0,0 +1,16 @@
+<?php
+require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."render.inc.php");
+?>
+
+    <section class="video">
+        <h2>Talk &amp; paper: <a href="http://xmllondon.com/2014/presentations/narmontas/" target="_blank">XML
+                Processing in Scala at XML London 2014</a></h2>
+
+        <p>Produced <a href="https://scala.contractors/xml_processing_in_scala.pdf">the paper</a> with <a
+                href="http://felstar.com/" target="_blank">Dino Fancellu</a> and presented it at the XML London 2014
+            conference.</p>
+        <iframe id="video" width="640" height="360" src="//www.youtube-nocookie.com/embed/sgDmXU5NHXU?rel=0"
+                frameborder="0" allowfullscreen></iframe>
+    </section>
+    
+    <?php echo $foot; 
