62dc7e10f9a533c59a170376e3eb46a9e3d2dc22
Vykintas Narmontas (William) <me@vynar.com>
2016-09-24 09:29:16 +0800

    Create index.php

---
+++ 1607/scala-set-up-quick-tutorial/index.php
@@ -0,0 +1,11 @@
+<?php
+require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."render.inc.php");
+?>
+    <section class="video">
+    	<h2>Tutorial: IntelliJ 2016, Scala 2.11, ScalaTest & SBT: Quick Tutorial</h2>
+        <iframe id="video" width="640" height="360" src="//www.youtube-nocookie.com/embed/A2w8kBA22vo?rel=0"
+                frameborder="0" allowfullscreen></iframe>
+
+
+    </section>
+<?php echo $foot;
