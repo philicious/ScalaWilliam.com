1067940296349697d252beffd22c8e3160d3e406
Vykintas Narmontas (William) <william@scalawilliam.com>
2016-11-26 22:27:16 +0800

    Actually just simplify GitHub instead.

--- index.php
+++ index.php
@@ -110,9 +110,23 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."rend
     </div>
     <aside id="links">
         <h2>Links</h2>
-        <p>
-            E-mail me:<a href="mailto:hello@scalawilliam.com" target="_blank" title="E-mail">hello@scalawilliam.com</a>
-        </p>
+        <nav>
+            <ul>
+                <li id="li-linkedin"><a href="https://uk.linkedin.com/in/scalawilliam/" target="_blank"
+                                        title="LinkedIn profile"
+                        ><h3>LinkedIn</h3>
+
+                        <p>See my professional profile and connect with me on LinkedIn.</p></a></li>
+                <li id="li-email"><a href="mailto:hello@scalawilliam.com" target="_blank" title="E-mail"
+                        ><h3>E-mail</h3>
+
+                        <p>Contact me to find out how I can help your business.</p></a></li>
+                <li id="li-github"><a href="https://github.com/ScalaWilliam/" target="_blank" title="GitHub link"
+                        ><h3>GitHub</h3>
+
+                        <p>See my code in open source projects..</p></a></li>
+            </ul>
+        </nav>
     </aside>
     <div class="section" id="scala">
         <section class="slide">
