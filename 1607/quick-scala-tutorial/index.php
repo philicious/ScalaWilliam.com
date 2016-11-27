97aef0c9d2bfa69f6215676461fa3ec659f298dd
Vykintas Narmontas <me@vynar.com>
2016-11-27 15:28:36 +0800

    Use meta property. Who knew?!

--- 1607/quick-scala-tutorial/index.php
+++ 1607/quick-scala-tutorial/index.php
@@ -7,11 +7,11 @@ require(dirname(__FILE__) . '/../../shared/render.inc.php');
     <meta property="og:locale" content="en_GB" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@ScalaWilliam" />
-    <meta name="og:title" content="Quick Scala Tutorial" />
-    <meta name="og:url" content="https://www.scalawilliam.com/1607/quick-scala-tutorial/" />
+    <meta property="og:title" content="Quick Scala Tutorial" />
+    <meta property="og:url" content="https://www.scalawilliam.com/1607/quick-scala-tutorial/" />
     <meta name="twitter:title" content="Quick Scala Tutorial" />
     <meta name="description" content="Essentials for doing Scala by William Narmontas (Scala William). Learn how to do TDD the proper way with ScalaTest and SBT, inside the standard IntelliJ Community Edition IDE." />
-    <meta name="og:description" content="Essentials for doing Scala by William Narmontas (Scala William). Learn how to do TDD the proper way with ScalaTest and SBT, inside the standard IntelliJ Community Edition IDE." />
+    <meta property="og:description" content="Essentials for doing Scala by William Narmontas (Scala William). Learn how to do TDD the proper way with ScalaTest and SBT, inside the standard IntelliJ Community Edition IDE." />
     <meta name="keywords" content="scala, sbt, intellij, scalatest, jvm, tdd" />
     <meta name="twitter:image" content="https://avatars3.githubusercontent.com/u/2464813" />
 
