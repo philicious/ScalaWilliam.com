b709c9a34ad25e1b3c13afd9aba24187199a7f31
Vykintas Narmontas (William) <me@vynar.com>
2016-12-14 18:21:06 +0800

    Update index.php

--- cv/index.php
+++ cv/index.php
@@ -5,13 +5,10 @@ require(dirname(__FILE__).'/../../shared/render.inc.php');
 <div class="section" id="william">
     <section class="slide">
         <p style="cursor:default">
-          &dash; <a href="https://docs.google.com/document/d/15-C0RFjc_E0t84xxmY9zGQc0phdnR4hniLpviiI98fc/export?format=pdf"><img
-                    src="pdficon_small.png"/> Download PDF</a><br/>
-          &dash; <a href="https://docs.google.com/document/d/15-C0RFjc_E0t84xxmY9zGQc0phdnR4hniLpviiI98fc/export?format=doc"
-          target="_blank"><img src="Microsoft-Word-2013-icon.png"/> Download Word document</a><br/>
             - <a href="https://docs.google.com/document/d/15-C0RFjc_E0t84xxmY9zGQc0phdnR4hniLpviiI98fc/edit" target="_blank">
-            <img src="kix-favicon6.ico"/> Google Docs document
-        </a>
+
+            <img src="kix-favicon6.ico"/> Google Docs document.</a> Can export <img src="Microsoft-Word-2013-icon.png" alt="Microsoft Word"/> and <img alt="PDF"
+                    src="pdficon_small.png"/>.
           </p>
         <iframe
                 src="https://docs.google.com/viewer?url=https://docs.google.com/document/d/15-C0RFjc_E0t84xxmY9zGQc0phdnR4hniLpviiI98fc/export?format%3Dpdf&id=15-C0RFjc_E0t84xxmY9zGQc0phdnR4hniLpviiI98fc&embedded=true"
