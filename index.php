80700f98f1828f25ff2e96068a0ea1587d9dcb2e
ScalaWilliam <william@scalawilliam.com>
2017-10-29 13:13:02 +0800

    Use a nicer looking timeline

--- index.php
+++ index.php
@@ -5,12 +5,9 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."rend
         <section class="slide">
             <span class="img"></span>
 
-		<p>I've been building websites and software since 2004.</p>
-		
-            <p>Since 2013, I've specialised in <a href="http://scala-lang.org"
-            target="_blank">Scala</a> and been IT consulting through <a href="http://www.aptelements.com/">Apt Elements</a>.
-		    Scala is a programming language for the JVM.
-		</p>
+		<p>Specialising in the <a href="http://scala-lang.org"
+            target="_blank">Scala</a> programming language since 2013.
+            <br/>Software development since 2004.</p>
 		
 		<p><a href="/cv/">Latest CV</a> &mdash;
                 <a href="#articles">Articles</a>
@@ -19,11 +16,11 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."rend
                 &mdash;
                 <a href="#talks">Talks</a>
                 &mdash;
-                <a href="#projects">Projects</a>.
+                <a href="#projects">Projects</a>
 
-                <br/>
+</p><p>
                 E-mail: <a href="mailto:hello@scalawilliam.com" target="_blank" title="E-mail"
-                >hello@scalawilliam.com</a>.
+                >hello@scalawilliam.com</a>
             </p>
 	</section>
     </div>
@@ -31,13 +28,13 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."rend
     <div class="section" id="talks">
         <section class="slide">
             <h2>Talks</h2>
-            <ul>
-                <li><a href="/scala-for-2017/">Scala for 2017</a>, Singapore, December 2016</li>
-                <li><a href="/1406/xml-processing-in-scala/">XML Processing in Scala,
-                        XML London 2014</a>, London, June 2014
+            <ul class="time-table">
+                <li><a href="/scala-for-2017/">Scala for 2017</a> <span class="time">Singapore, December 2016</span></li>
+                <li><a href="/1406/xml-processing-in-scala/">XML Processing in Scala 
+                        XML London 2014</a> <span class="time">London, June 2014</span>
                 </li>
-                <li><a href="/1404/an-introduction-to-scala/">An Introduction to Scala</a>, City of London,
-                    April 2014
+                <li><a href="/1404/an-introduction-to-scala/">An Introduction to Scala</a> <span class="time">City of London,
+                    April 2014</span>
                 </li></ul>
         </section>
     </div>
@@ -46,62 +43,61 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."rend
         <section class="slide">
             <h2 class="p-name"><data class="u-url" value="https://www.scalawilliam.com/#articles">Articles</data></h2>
             <a class="u-author" href="/" style="display:none"></a><!-- Help parsers out. The author of all of these articles is https://www.scalawilliam.com/ -->
-            <p>Also on <a href="https://plus.google.com/collection/oFuHlB" target="_blank">Google+</a>.</p>
-            <ul>
+            <ul class="time-table">
                 <li class="h-entry">
-                    <a class="p-name" href="/unit-testing-is-simple/">Unit testing is simple</a>,
+                    <a class="p-name" href="/unit-testing-is-simple/">Unit testing is simple</a>
                     <time class="dt-published" datetime="2017-05-04">May 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1705/advantages-of-scala-over-kotlin/">What are advantages of Scala over Kotlin?</a>,
+                    <a class="p-name" href="/1705/advantages-of-scala-over-kotlin/">What are advantages of Scala over Kotlin?</a>
                     <time class="dt-published" datetime="2017-05-29">May 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/web-standards-for-content-syndication/">Web standards for content syndication</a>,
+                    <a class="p-name" href="/web-standards-for-content-syndication/">Web standards for content syndication</a>
                     <time class="dt-published" datetime="2017-05-21">May 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1705/scala-android-opportunity/">Why Scala didn't miss the Android opportunity</a>,
+                    <a class="p-name" href="/1705/scala-android-opportunity/">Why Scala didn't miss the Android opportunity</a>
                     <time class="dt-published" datetime="2017-05-18T11:41:55.686Z">May 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1705/greenfield-technical-debt/">How I deal with greenfield technical debt</a>,
+                    <a class="p-name" href="/1705/greenfield-technical-debt/">How I deal with greenfield technical debt</a>
                     <time class="dt-published" datetime="2017-05-05T10:47:11.753Z">May 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1704/certifications-im-considering/">Certifications I'm considering, as an experienced software engineer</a>,
+                    <a class="p-name" href="/1704/certifications-im-considering/">Certifications I'm considering, as an experienced software engineer</a>
                     <time class="dt-published" datetime="2017-04-29">Apr 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1704/2017-freelance-development-platforms/">2017 freelance development platforms</a>,
+                    <a class="p-name" href="/1704/2017-freelance-development-platforms/">2017 freelance development platforms</a>
                     <time class="dt-published" datetime="2017-04-20">Apr 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/scala-native-libpcap/">Capturing Packets with Scala Native and libpcap</a>,
+                    <a class="p-name" href="/scala-native-libpcap/">Capturing Packets with Scala Native and libpcap</a>
                     <time class="dt-published" datetime="2017-03-26">Mar 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1701/nashorn-cli-use-case/">Java 8 Nashorn CLI use case: prepending ISO instant to CSV from UNIX timestamp</a>,
+                    <a class="p-name" href="/1701/nashorn-cli-use-case/">Java 8 Nashorn CLI use case: prepending ISO instant to CSV from UNIX timestamp</a>
                     <time class="dt-published" datetime="2017-01-02T03:05:17.144Z">Jan 2017</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/scala-nashorn-interaction/">Scala Nashorn Interaction</a>,
+                    <a class="p-name" href="/scala-nashorn-interaction/">Scala Nashorn Interaction</a>
                     <time class="dt-published" datetime="2016-12-29">Dec 2016</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1612/limit-degrees-of-freedom/">Limit degrees of freedom in development</a>,
+                    <a class="p-name" href="/1612/limit-degrees-of-freedom/">Limit degrees of freedom in development</a>
                     <time class="dt-published" datetime="2016-12-17T11:21:25.101Z">Dec 2016</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/most-important-streaming-abstraction/">The most important Streaming abstraction</a>,
+                    <a class="p-name" href="/most-important-streaming-abstraction/">The most important Streaming abstraction</a>
                     <time class="dt-published" datetime="2016-12-14">Dec 2016</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/essential-sbt/">Essential SBT</a>,
+                    <a class="p-name" href="/essential-sbt/">Essential SBT</a>
                     <time class="dt-published" datetime="2016-12-10">Dec 2016</time>
                 </li>
                 <li class="h-entry">
-                    <a class="p-name" href="/1609/feature-switches-agile-scala-jmx/">Feature Switches, Inheritance and Agile with Scala &amp; JMX on the JVM</a>,
+                    <a class="p-name" href="/1609/feature-switches-agile-scala-jmx/">Feature Switches, Inheritance and Agile with Scala &amp; JMX on the JVM</a>
                     <time class="dt-published" datetime="2016-09-30T12:38:59.026Z">Sep 2016</time>
                 </li>
             </ul>
@@ -111,34 +107,35 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."rend
     <div class="section" id="projects">
         <section class="slide">
             <h2>Open Source projects</h2>
-            <ul id="os-projects-list">
-		    <li><a href="https://github.com/ScalaWilliam/scalajs-cli-demo" target="_blank">scalajs-cli-demo</a>, May 2017 to now.</li>
-		    <li><a href="https://github.com/ScalaWilliam/eventsource-hub" target="_blank">EventSource Hub</a>, April 2017 to now.</li>
-		    <li><a href="https://github.com/ScalaWilliam/express-scalajs-nodejs-demo" target="_blank">Express on ScalaJS on NodeJS</a>, April 2017.</li>
-                <li><a href="https://git.watch/" target="_blank">Git Watch</a>, July 2016 to now.</li>
-                <li><a target="_blank" href="https://github.com/ScalaWilliam/ActionFPS">ActionFPS</a>,
-                    January 2014 to now.
+            <ul id="os-projects-list" class="time-table">
+		    <li><a href="https://github.com/ScalaWilliam/scalajs-cli-demo" target="_blank">scalajs-cli-demo</a> <span class="time">May 2017 to now</span></li>
+		    <li><a href="https://github.com/ScalaWilliam/eventsource-hub" target="_blank">EventSource Hub</a> <span class="time">April 2017 to now</span></li>
+		    <li><a href="https://github.com/ScalaWilliam/express-scalajs-nodejs-demo" target="_blank">Express on ScalaJS on NodeJS</a> <span class="time">April 2017</span></li>
+                <li><a href="https://git.watch/" target="_blank">Git Watch</a> <span class="time">July 2016 to now</span></li>
+                <li><a target="_blank" href="https://github.com/ScalaWilliam/ActionFPS">ActionFPS</a> <span class="time">January 2014 to now</span>
                 </li>
                 <li><a target="_blank" href="https://github.com/ScalaWilliam/amazon-wishlist-api">Amazon Wishlist API
-                        for Scala with Play</a>, July 2013 to now.
+                        for Scala with Play</a>
+                        <span class="time">July 2013 to now</span>
                 </li>
-                <li><a target="_blank" href="https://github.com/ScalaWilliam/xs4s">XML Streaming for Scala (xs4s)</a>,
-                    December 2013 to now.
+                <li><a target="_blank" href="https://github.com/ScalaWilliam/xs4s">XML Streaming for Scala (xs4s)</a>
+                <span class="time">December 2013 to now</span>
                 </li>
-                <li><a target="_blank" href="https://github.com/ScalaWilliam/duel.gg">duel.gg</a>, January 2014 to now.
+                <li><a target="_blank" href="https://github.com/ScalaWilliam/duel.gg">duel.gg</a>
+                <span class="time">January 2014 to now</span>
                 </li>
-                <li><a target="_blank" href="https://github.com/ScalaWilliam/xp4s">XML Printing for Scala</a>,
-                    December 2013 to now.
+                <li><a target="_blank" href="https://github.com/ScalaWilliam/xp4s">XML Printing for Scala</a>
+                <span class="time">December 2013 to now</span>
                 </li>
-                <li><a target="_blank" href="https://github.com/ScalaWilliam/sygments">Sygments: Pygments for Scala</a>,
-                    June 2014 to June 2016.
+                <li><a target="_blank" href="https://github.com/ScalaWilliam/sygments">Sygments: Pygments for Scala</a>
+                <span class="time">June 2014 to June 2016</span>
                 </li>
-                <li><a target="_blank" href="http://pastebox.sf.net">PasteBox: Minimalist PHP Pastebin</a>,
-                    February 2007 to July 2009.
+                <li><a target="_blank" href="http://pastebox.sf.net">PasteBox: Minimalist PHP Pastebin</a>
+                <span class="time">February 2007 to July 2009</span>
                 </li>
 
-                <li><a href="http://www.milanohousecurtains.co.uk/" target="_blank">Milano House Curtains</a>, August
-                    2012 to now
+                <li><a href="http://www.milanohousecurtains.co.uk/" target="_blank">Milano House Curtains</a> August
+                <span class="time">2012 to now</span>
                 </li>
             </ul>
         </section>
@@ -146,28 +143,28 @@ require(dirname(__FILE__).DIRECTORY_SEPARATOR."shared".DIRECTORY_SEPARATOR."rend
     <div class="section" id="soc-clubs">
         <section class="slide">
             <h2>Societies &amp; clubs</h2>
-            <ul id="societies-clubs">
-	        <li><a href="http://www.camsoclon.org/" target="_blank">Cambridge Society of London</a>,
-	        member since November 2013</li>
-                <li><a href="https://www.cambridgelibertarians.org.uk/" target="_blank">Cambridge Libertarians</a>,
-                    founded in March 2011, president until November 2011
+            <ul class="time-table" id="societies-clubs">
+	        <li><a href="http://www.camsoclon.org/" target="_blank">Cambridge Society of London</a>
+	        <span class="time">member since November 2013</span></li>
+                <li><a href="https://www.cambridgelibertarians.org.uk/" target="_blank">Cambridge Libertarians</a>
+                    <span class="time">founded in March 2011, president until November 2011</span>
                 </li>
-                <li><a href="http://mensa.org.uk/" target="_blank">British Mensa</a>, member since May 2014</li>
+                <li><a href="http://mensa.org.uk/" target="_blank">British Mensa</a> <span class="time">member since May 2014</span></li>
             </ul>
         </section>
     </div>
     <div class="section" id="other-projects">
         <section class="slide">
             <h2>Other projects</h2>
-            <ul id="projects-list">
+            <ul class="time-table" id="projects-list">
                 <li><a href="http://hi-skill.us/" target="_blank">HI-SKILL rankings</a> for <a
-                            href="http://assault.cubers.net/" target="_blank">AssaultCube</a>, June 2009 to December 2010
+                            href="http://assault.cubers.net/" target="_blank">AssaultCube</a> <span class="time">June 2009 to December 2010</span>
                 </li>
-                <li><a href="http://acwc.woop.us/acwc0709/" target="_blank">AssaultCube World Cup 2009</a>, June 2009 to
-                    July 2009
+                <li><a href="http://acwc.woop.us/acwc0709/" target="_blank">AssaultCube World Cup 2009</a> <span class="time">June 2009 to
+                    July 2009</span>
                 </li>
-                <li><a href="http://acwc.woop.us/2010/" target="_blank">AssaultCube World Cup 2010</a>, June 2010 to
-                    August 2010
+                <li><a href="http://acwc.woop.us/2010/" target="_blank">AssaultCube World Cup 2010</a> <span class="time">June 2010 to
+                    August 2010</span>
                 </li>
             </ul>
         </section>
