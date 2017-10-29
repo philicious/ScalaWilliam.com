db374a570b3ac8bf074dab36abfb6bd9d35fa25f
ScalaWilliam <william@scalawilliam.com>
2017-10-29 10:45:39 +0800

    Fix broken link

--- 1412/amazon-wishlist-api/index.php
+++ 1412/amazon-wishlist-api/index.php
@@ -3,7 +3,7 @@ require(dirname(__FILE__).'/../../shared/render.inc.php');
 ?>
 
 <h1>Amazon Wish List API for Scala</h1>
-<h2>By <a href="https://scalawilliam.com/">William Narmontas</a>, Polyglot Software Engineer</h2>
+<h2>By <a href="https://www.scalawilliam.com/">William Narmontas</a>, Polyglot Software Engineer</h2>
 </header>
 <p>Sample project showing how to extract items from your Amazon Wishlist, turn them into JSON, serve them via Spray, to an AngularJS interface, all looked after by SBT 0.13.7 and its native packager plugin, under Scala 2.11.4</p>
 <div class="section" id="william">
