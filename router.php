f9cc6972dd32e50f421758cf312be136f68fe1a1
William Narmontas <me@vynar.com>
2017-11-04 07:54:40 +0800

    Add what-is-scala

--- router.php
+++ router.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'vendor/autoload.php';
 $uri = $_SERVER['REQUEST_URI'];
 if ( $uri == "/sitemap/" ) {include("sitemap.php");}
 else if ( $uri == "/sitemap.xml") { include("sitemap-xml.php"); }
